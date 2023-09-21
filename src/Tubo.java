d3b0150a0940ed7573a2138a73a313513aea1ef7
samuel.rossi <samuel.rossi@buonarroti.tn.it>
2023-09-21 14:35:21 +0200

    feat: metodo costruttore

--- src/Tubo.java
+++ src/Tubo.java
@@ -2,4 +2,9 @@ public class Tubo {
     private float altezza;
     private float diametro;
     final float pi=(float) 3.1415;
+    
+    public void Tubo(float h, float d){
+        diametro=d;
+        altezza=h;
+    }
 }
